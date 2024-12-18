f48e53b8a5ccf6f899ed14e4fee84c441961aaf7
jun <912833721@qq.com>
2024-12-19 03:13:56 +0800

    升级agp、kotlin plugin，使其能够智能提示出来。同时把原来MainActivity的逻辑迁移到MyComponentActivity

--- Chapter_02_BasicUI/Demo_Bloom/BloomCompose/app/src/main/java/com/example/compose/MyComponentActivity.kt
+++ Chapter_02_BasicUI/Demo_Bloom/BloomCompose/app/src/main/java/com/example/compose/MyComponentActivity.kt
@@ -1,88 +1,62 @@
-/*
- * Copyright 2021 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.compose
 
-import CounterComponent
-import Custom_Layout.IntrinsicDemo
-import Custom_Layout.IntrinsicRow
-import Custom_Layout.SubcompositionDemo
-import Layout_UI.ConstraintLayoutDemo
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.compose.ui.HomePage
 import com.example.compose.ui.LoginPage
 import com.example.compose.ui.WelcomePage
 import com.example.compose.ui.theme.BloomTheme
-import CompositionLocal.*
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.snapshotFlow
-import gesture.BaseDragGestureDemo
-import gesture.ClickDemo
-import gesture.CombinedClickDemo
-import gesture.DragDemo
-import gesture.DragGestureDemo
-import gesture.DragPlayGround
-import gesture.DragToDismiss
-import gesture.PaddingDemo
-import gesture.SwipeableDemo
-import gesture.TapGestureDemo
-import gesture.TransformGestureDemo
-import gesture.TransformerDemo
-import kotlinx.coroutines.flow.collect
-import androidx.compose.runtime.*
-import androidx.compose.ui.tooling.preview.Preview
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
 
-class MainActivity : AppCompatActivity() {
-
+class MyComponentActivity : ComponentActivity() {
     var theme: BloomTheme by mutableStateOf(BloomTheme.LIGHT)
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        // window.setDecorFitsSystemWindows(false)
         super.onCreate(savedInstanceState)
         setContent {
-            // WelcomePage()
-            // LoginPage()
-            // HomePage()
-            // SubcompositionDemo()
-            // ConstraintLayoutDemo()
-            // CounterComponent()
-            // IntrinsicDemo()
-            // BaseDragGestureDemo()
-            // DragGestureDemo()
-            // TapGestureDemo()
-            // TransformGestureDemo()
-            // DragToDismiss()
-            // PaddingDemo()
-            // CompositionLocalScreen()
-            DefaultPreview2()
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.primary
+                ) {
+                    // WelcomePage()
+                    // LoginPage()
+                    HomePage()
+                    // SubcompositionDemo()
+                    // ConstraintLayoutDemo()
+                    // CounterComponent()
+                    // IntrinsicDemo()
+                    // BaseDragGestureDemo()
+                    // DragGestureDemo()
+                    // TapGestureDemo()
+                    // TransformGestureDemo()
+                    // DragToDismiss()
+                    // PaddingDemo()
+                    // CompositionLocalScreen()
+                    // DefaultPreview2()
+                }
         }
     }
 }
+
 
 @Composable
 fun WelcomePageLightPreview() {
